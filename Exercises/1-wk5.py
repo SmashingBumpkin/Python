3db2e00e1c9efdb10251cb5d9881ca107e4bffc1
SmashingBumpkin <66125276+SmashingBumpkin@users.noreply.github.com>
2022-10-24 17:20:49 +0200

    Update 1-wk5.py

--- Exercises/1-wk5.py
+++ Exercises/1-wk5.py
@@ -42,18 +42,43 @@ def Binary2Decimal(bin_list: list) -> list:
     
     return decList
 
+
+# assume you are given a list of single characters (i.e., every list item
+# is a 1 character-long string) and an integer value k > 0.
+# write a function that countes and returns the number of characters
+# in the list that repeat k or more times consecutively.
+
+# for example, if the list is ['a', 'a', 'z', 'b', 'b', 'b', 'a', 'a', 'a', 'z', 'c', 'c']
+# for k = 1, the fuction returns 6, as all characters repeat al least one time;
+# for k = 2, the function returs 4, as a, b, a and c repeat 2, 3, 3, 2 times, respectively;
+# for k = 3, the function returns 2, as b and a repeat 3, 3 times, respectively;
+# for k = 4 (or more), the function returns 0.
+
 def RepeatingChars(charList : list, k : int) -> int:
-  #your code goes here
-  pass
-
-
-if __name__ == "__main__":
-  print(RepeatingChars(['a', 'a', 'z', 'b', 'b', 'b', 'a', 'a', 'a', 'z', 'c', 'c'], 1))
-  print(RepeatingChars(['a', 'a', 'z', 'b', 'b', 'b', 'a', 'a', 'a', 'z', 'c', 'c'], 2))
-  print(RepeatingChars(['a', 'a', 'z', 'b', 'b', 'b', 'a', 'a', 'a', 'z', 'c', 'c'], 3))
-  print(RepeatingChars(['a', 'a', 'z', 'b', 'b', 'b', 'a', 'a', 'a', 'z', 'c', 'c'], 4))
+    #loop through each element of charList
+    #count +1
+    #on next loop, if element = previous element, count +1
+    #else append count to list of counts
+    #end loop and return # elements in count greater than k
+    lastChar = ""
+    consecList = []
+    count = 0
+    for i in charList:
+        if i == lastChar:
+            count += 1
+        else:
+            consecList.append(count)
+            count = 1
+            lastChar = i
+    consecList.append(count)
+    return len([i for i in consecList if i >= k])
+          
 
 
 if __name__ == "__main__":
   print(Binary2Decimal_Value("01110100")) # 116
   print(Binary2Decimal(["00", "01", "10", "11"])) # [0, 1, 2, 3]
+  print(RepeatingChars(['a', 'a', 'z', 'b', 'b', 'b', 'a', 'a', 'a', 'z', 'c', 'c'], 1))
+  print(RepeatingChars(['a', 'a', 'z', 'b', 'b', 'b', 'a', 'a', 'a', 'z', 'c', 'c'], 2))
+  print(RepeatingChars(['a', 'a', 'z', 'b', 'b', 'b', 'a', 'a', 'a', 'z', 'c', 'c'], 3))
+  print(RepeatingChars(['a', 'a', 'z', 'b', 'b', 'b', 'a', 'a', 'a', 'z', 'c', 'c'], 4))
