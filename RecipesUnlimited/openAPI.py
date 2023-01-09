0477b1d5bf672d778aa56b039417460ea967bb2d
SmashingBumpkin <66125276+SmashingBumpkin@users.noreply.github.com>
2023-01-09 13:58:02 +0100

    Improved api

--- RecipesUnlimited/openAPI.py
+++ RecipesUnlimited/openAPI.py
@@ -13,10 +13,6 @@ Created on Mon Dec 26 15:33:44 2022
 #api key: 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
 
 #openai.organization = "YOUR_ORG_ID"
-def cupboardImporter():
-    mycupboard = kitchenManager.Cupboard("kitchenContents.db")
-    print(mycupboard)
-    return mycupboard
 
 def ingredientReturner(cupboard):
     return ", ".join(cupboard.returnLongLifeNames())
@@ -55,32 +51,26 @@ def perishableAdder(cupboard):
     
     return mustUse
 
-def recipebookImporter():
-    myrecipes = kitchenManager.Cupboard("kitchenContents.db")
-    print(myrecipes)
-    return myrecipes
-
-
 def optionsPrompt(mustUse, canUse):
-    myprompt = ("Provide a 1-2 sentence overview of 5 recipes sentences which"
-                + " notes all it's key ingredients once. The recipe may only "
-                + "use ingredients that are listed below:\n" + canUse
-                + "\n\nThe suggestions must use at least one of these ingredients:\n"
-                + mustUse +"\n\n")
+    myprompt = ("Provide a 1-2 sentence overview of 5 varied recipes which"
+                + " note all the key ingredients once. The suggestions must use "
+                + "at least one of these ingredients:\n"
+                + mustUse + "\n\nThe recipe can also use any ingredients that are listed below:\n"
+                + canUse +"\n\n")
     print(myprompt)
     return myprompt
 
 def optionsPromptBroad(mustUse, canUse):
-    myprompt = ("Provide a 1-2 sentence overview of 5 recipes sentences which"
-                + " notes all it's key ingredients once. The recipe can use "
-                + "ingredients that are listed below:\n" + canUse
-                + "\n\nThe suggestions must use at least one of these ingredients:\n"
-                + mustUse +"\n\nThe suggestions should also use one or two "
-                + "ingredients that aren't listed here.")
+    myprompt = ("Provide a 1-2 sentence overview of 5 varied recipes which"
+                + " note all the key ingredients once. The suggestions must use "
+                + "at least one of these ingredients:\n"
+                + mustUse + "\n\nThe recipe can also use any ingredients that are listed below:\n"
+                + canUse +"\n\nThe suggestions should also use one or two "
+                + "ingredients that aren't listed here.\n")
     print(myprompt)
     return myprompt
 
-def sendPrompt(myprompt):
+def sendPrompt(myprompt, temperature = 0.01):
     cont = input("\n____\n\n\nPlease type 1 to continue (will spend openai credits) ")
     if cont != "1":
         print("\nYou have terminated the program, cheapskate.\n")
@@ -90,7 +80,7 @@ def sendPrompt(myprompt):
       model="text-davinci-003",
       prompt=myprompt,
       max_tokens=600,
-      temperature=0.01
+      temperature=temperature
     )
     return response["choices"][0]["text"]
 
@@ -102,31 +92,29 @@ def optionChooser(options):
 
 def recipePrompt(outline, mustUse, canUse):
     myprompt = ("Write a recipe for this dish: \n\n" + outline
-                + "\nthe recipe can include any of these ingredients:\n"
-                + canUse + mustUse)
+                + "\nthe recipe may include any of these ingredients:\n"
+                + canUse + mustUse + "\n\nIgnore any ingredients that "
+                + "don't fit in the recipe.")
     
     print("\n_______________\n\n\n" + myprompt)
     return myprompt
 
-def recipebookImporter():
-    myrecipes = kitchenManager.RecipeBook("cupboardContents.db")
-    return myrecipes
 
 def recipeAdder(recipebook, recipe):
     #adds recipe to recipe book
     cont = int(input("\n\nType 1 to add the recipe to the recipe book: "))
     
     if cont:
-        recipebook.addRecipe(Recipe.textToRecipe(recipe))
+        recipebook.addRecipe(kitchenManager.Recipe.textToRecipe(recipe))
         print("Recipe added")
     else:
         print("\nRecipe not added.")
     
-cupboard = cupboardImporter()
+cupboard = kitchenManager.Cupboard("kitchenContents.db")
 canUse = ingredientReturner(cupboard)
 mustUse = perishableAdder(cupboard)
-myprompt = optionsPromptBroad(mustUse,canUse)
-options = sendPrompt(myprompt)
+myprompt = optionsPrompt(mustUse,canUse)
+options = sendPrompt(myprompt, temperature = 0.3)
 """options = \n1. Penne with Olive Oil, Butter, White Pepper, and 
 Rosemary: A classic Italian dish with a twist, this recipe combines penne 
 pasta with olive oil, butter, white pepper, and rosemary for a delicious 
@@ -150,5 +138,5 @@ recipe = sendPrompt(recipeRequest)
 #recipe="\n\nPenne Alfredo with Black Mustard Seeds\n\nIngredients:\n\n- 1 package penne pasta\n- 2 tablespoons olive oil\n- 2 tablespoons vegetable oil\n- 2 tablespoons black mustard seeds\n- 2 cloves garlic, minced\n- 2 tablespoons ground cumin\n- 1 tablespoon dried basil\n- 1 tablespoon garam masala\n- 1 tablespoon paprika\n- 1 teaspoon turmeric\n- 1 teaspoon black pepper\n- 1 cup cooked basmati rice\n- 2 tablespoons pau bhaji\n- 1/4 cup pine nuts\n- 1/4 cup sesame seeds\n- 1/2 cup milk\n- 2 tablespoons butter\n- 2 eggs\n- 1 tablespoon rosemary, chopped\n\nInstructions:\n\n1. Bring a large pot of salted water to a boil and cook the penne according to package instructions. Drain and set aside.\n\n2. Heat the olive oil and vegetable oil in a large skillet over medium heat. Add the mustard seeds and cook until they start to pop, about 1 minute.\n\n3. Add the garlic, cumin, basil, garam masala, paprika, and turmeric and cook for another minute.\n\n4. Add the rice and pau bhaji and cook for 3-4 minutes, stirring occasionally.\n\n5. Add the pine nuts, sesame seeds, milk, butter, and eggs and cook until the eggs are cooked through, about 4 minutes.\n\n6. Add the penne and rosemary and toss to combine.\n\n7. Serve warm, topped with additional sesame seeds, if desired."
 
 print(recipe)
-recipebook = recipebookImporter()
+recipebook = kitchenManager.RecipeBook("kitchenContents.db")
 recipeAdder(recipebook, recipe)
