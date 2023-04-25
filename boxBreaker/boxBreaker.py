4739db42479bb9788140adba3e2c4e39c72da140
SmashingBumpkin <66125276+SmashingBumpkin@users.noreply.github.com>
2023-04-25 17:57:27 +0200

    fixed edge cases and slight improvement

--- boxBreaker/boxBreaker.py
+++ boxBreaker/boxBreaker.py
@@ -1,7 +1,7 @@
 import math 
-n = 25**2 #number of floors
+n = 500**2 #number of floors
 k = 2 #number of boxes
-iStar = 50
+iStar = 1000
 
 def BoxBreaker(k, n, iStar):
     maxTests = round(2*(n**0.5))
@@ -9,27 +9,31 @@ def BoxBreaker(k, n, iStar):
 
 
 
-def BoxBreakerHelper(k, n, iStar, testHeight, safeHeight, attempts):
+def BoxBreakerHelper(k, n, iStar, testHeight, safeHeight, attempts, failedHeight = 0):
     attempts += 1
     print(str(k) + " " + str(n)+ " "  + str(iStar)+ " test: "  + str(testHeight)+ " safe: " + str(safeHeight))
     if k == 1:
-        for floor in range(safeHeight + 1, n+1):
-            attempts += 1
+        for floor in range(safeHeight + 1, failedHeight):
             if floor == iStar:
                 return (floor, attempts)
+            attempts += 1
+        return (failedHeight, attempts)
     if testHeight < iStar:
+        
         safeHeight = testHeight
         jeff = n-testHeight
         if jeff <=5:
-            testHeight = safeHeight + math.floor(jeff*0.5)
+            if jeff == 1:
+                testHeight = n
+            else:
+                testHeight = safeHeight + math.floor(jeff*0.5)
         else:
             testHeight = safeHeight + 2*math.floor(jeff**0.5)
-            
             if testHeight == safeHeight:
                 testHeight += 1
         return BoxBreakerHelper(k, n, iStar, testHeight, safeHeight, attempts)
     else:
-        return BoxBreakerHelper(k-1 , n, iStar, safeHeight + 1, safeHeight, attempts)
+        return BoxBreakerHelper(k-1 , n, iStar, safeHeight + 1, safeHeight, attempts, failedHeight = testHeight)
 
 
 
