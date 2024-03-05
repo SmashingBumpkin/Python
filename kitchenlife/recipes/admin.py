e8a3b04e33bdccfec97d4cc4d0c5a9fdb5c32b1d
SmashingBumpkin <66125276+SmashingBumpkin@users.noreply.github.com>
2024-03-05 09:50:06 +0100

    tidy

--- kitchenlife/recipes/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import ProfileIngredient, Recipe, Profile, RecipeIngredient
-
-admin.site.register(Recipe)
-admin.site.register(Profile)
-admin.site.register(RecipeIngredient)
-admin.site.register(ProfileIngredient)
--- kitchenlife/recipes/templatetags/custom_filters.py
+++
@@ -1,6 +0,0 @@
-from django import template
-register = template.Library()
-
-@register.filter
-def scaled_string_with_arg(recipe_ingredient, scale):
-    return recipe_ingredient.scaled_string(scale)
